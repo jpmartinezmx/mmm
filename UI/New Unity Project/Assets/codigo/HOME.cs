5b9b45d9d5dc01200eee12a1d7f99c61a1faf667
cristinakunst <cristinabriones.n@gmail.com>
2016-11-17 11:42:17 -0600

    ultimo intento

--- UI/New Unity Project/Assets/codigo/HOME.cs
+++ UI/New Unity Project/Assets/codigo/HOME.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class HOME : MonoBehaviour {
 	public void ClickClick (){
 		Debug.Log ("clicked");
+		Application.LoadLevel ("HOME");
 	}
 	public void ClickTest2 (string text){
 		Debug.Log (text);
