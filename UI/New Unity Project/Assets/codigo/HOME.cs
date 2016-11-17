c8e1063112c873a553e336616e556d4bd4e883ee
cristinakunst <cristinabriones.n@gmail.com>
2016-11-17 09:06:45 -0600

    todos los ui

---
+++ UI/New Unity Project/Assets/codigo/HOME.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum Estado  
+{
+	normal,
+	hover,
+	armed
+
+}
+
+public class HOME : MonoBehaviour {
+	
+	
+
+	// Use this for initialization
+	void Start () {
+		
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		/*AddL(() => 
+			{
+				Application.LoadLevel ("HOME.cs");
+			});*/
+		
+	
+	}
+}
+
+
+
+
+
