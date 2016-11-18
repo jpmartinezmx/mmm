b91e772798f29b14ba1e5f7e3c8ca29a4ab07c10
MarceMtz <mmgomez0093@gmail.com>
2016-11-18 15:54:02 -0600

    viernes2

---
+++ Proyecto Unity/Unity Files/Assets/Marce/scriptsM/girar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class girar : MonoBehaviour {
+
+	public float distancia = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (gameObject.name == "circulo1") {
+			transform.Rotate (Vector3.down * Time.deltaTime * 30.0f);
+		}
+
+		if (gameObject.name == "circulo2") {
+			transform.Rotate (Vector3.up * Time.deltaTime * 100.0f);
+		}
+
+		if (gameObject.name == "circulo3") {
+			transform.Rotate (Vector3.down * Time.deltaTime * 50.0f);
+		}
+
+		if (gameObject.name == "circulo4") {
+			transform.Rotate (Vector3.down * Time.deltaTime * 5.0f);
+		}
+	}
+}
